73b8b62a31213844b45ab74deee4b43e33f79071
DashaPixie2 <darya.designlife@gmail.com>
2025-04-01 13:25:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,26 @@ app.post('/send-email', upload.fields([{ name: 'idFront' }, { name: 'idBack' }])
                     .text(`Birthday: ${birthday}`)
                     .moveDown();
 
-    doc.text(`I am not a hemophiliac...`).moveDown(); // here insert full legal text
+    doc.text(`I am not a hemophiliac (bleeder). I do not have Diabetes, Epilepsy, Hepatitis, Aids or any other communicable disease. 
+      I am not under the influence of alcohol and or drugs.
+
+      I acknowledge it is not reasonably possible for Dasha Pixie to determine whether I might have an allergic reaction to the pigments or process used in my Tattoo,
+      and I agree to accept the risk that such a reaction is possible.
+
+      I acknowledge that infection is always possible as a result of obtaining a Tattoo, particularly in the event that I do not take proper care of my Tattoo, 
+      and I agree to follow all instructions concerning the care of my own Tattoo while it is healing. 
+      I agree That any touch-up work needed due to my own negligence will be done at my own expense.
+
+      I realize that variations in color and design may exist between any tattoo as selected by Me and as ultimately applied to my body. 
+      I understand that if my skin color is dark, the Colors will not appear as bright as they do on light skin.
+
+      I acknowledge a Tattoo is a permanent change to my appearance and no representations have been made to me regarding the ability to later change or remove my tattoo. 
+      To my knowledge, I do not have any physical, mental, medical impairment or disability, which might affect my well-being as a direct or indirect result of my decision to have any tattoo-related work done at this time.
+
+      I acknowledge that I have truthfully represented to Dasha Pixie that I am 18 years old, and the following information is true and correct.
+      I acknowledge obtaining of my tattoo is by my choice alone and I consent to the application of the tattoo and to any action or conduct of Dasha Pixie reasonably necessary to perform the tattoo procedure.
+
+      I agree to release and forever discharge and hold harmless Dasha Pixie from any and all claims, damages, and legal actions arising from or connected in any way with my tattoo of the procedures and conduct used to apply my Tattoo.`).moveDown(); // here insert full legal text
 
     if (signature) {
         try {
@@ -83,7 +102,13 @@ app.post('/send-email', upload.fields([{ name: 'idFront' }, { name: 'idBack' }])
 
             addSignatureBlock(doc, signaturePath);
 
-            doc.text(`I confirm that the signature provided is my own...`).moveDown(); // Insert full confirmation legal text
+            doc.text(`I confirm that the signature provided is my own, created by me personally and electronically. 
+I acknowledge that this electronic signature is legally binding in accordance with the U.S. Electronic Signatures in Global and National Commerce Act (E-Sign Act) 
+and Uniform Electronic Transactions Act (UETA). By clicking/tapping/touching/selecting or otherwise interacting with the "Submit" button below, 
+you are consenting to signing this Document electronically. You agree your electronic signature ("E-Signature") is the legal equivalent of your manual signature 
+on this Document. You consent to be legally bound by this Document's agreement(s), acknowledgement(s), policy(ies), disclosure(s), consent term(s) and condition(s). 
+You agree that no certification authority or other third party verification is necessary to validate your E-Signature and that the lack of such certification 
+or third party verification will not in any way affect the enforceability of your E-Signature. You may request a paper version of an electronic record by writing to us.`).moveDown(); // Insert full confirmation legal text
             addSignatureBlock(doc, signaturePath);
 
             fs.unlinkSync(signaturePath);
