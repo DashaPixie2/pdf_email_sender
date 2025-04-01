415a775168da2579c9344ab4c8d08e04ee18e2ab
DashaPixie2 <darya.designlife@gmail.com>
2025-04-01 13:30:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -151,4 +151,12 @@ or third party verification will not in any way affect the enforceability of you
             }
 
             console.log('✅ Email sent successfully:', info.response);
-            res.send('E
+            res.send('Email sent successfully');
+        });
+    });
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
